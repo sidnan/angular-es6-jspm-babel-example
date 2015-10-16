98026f727f9eafd4cc80973e2f732ec3786403e6
sidnan <sid.nandhan@gmail.com>
2015-10-15 22:06:47 -0400

    clean up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,18 @@ var gulp                    = require('gulp');
 var shell                    = require('gulp-shell');
 
 
-// intialize browserSync
-// browserSync.create();
-
-
-gulp.task('jspm:bundle', shell.task([
-  'jspm bundle app dist/main.js'
-]))
 
 gulp.task('jspm:bundle-sfx', shell.task([
   'jspm bundle-sfx app dist/app.sfx.js'
 ]))
 
-// gulp.task('watch:js', ['jspm:bundle', 'jspm:bundle-sfx'], browserSync.reload);
-
-// gulp.task('serve', () => {
-//     browserSync.init({ server: { baseDir: "./" }});
-//     gulp.watch('./**/*.js', ['watch:js']);
-//     gulp.watch('./**/*.html', browserSync.reload);
-// });
 
 gulp.task('serve-changes', () => {
-    gulp.watch('./**/*.js', ['jspm:bundle', 'jspm:bundle-sfx']);
+    gulp.watch('./**/*.js', ['jspm:bundle-sfx']);
 });
 
 gulp.task('start-server', shell.task([
   'live-server'
 ]))
 
-// gulp.task('default', ['jspm:bundle-sfx', 'jspm:bundle', 'serve']);
-gulp.task('default', ['jspm:bundle-sfx', 'jspm:bundle', 'serve-changes', 'start-server']);
+gulp.task('default', ['jspm:bundle-sfx', 'serve-changes', 'start-server']);
