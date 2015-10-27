6deaa5c68fbdcedb46304877f0527d70407d980d
sidnan <sid.nandhan@gmail.com>
2015-10-26 21:16:31 -0400

    gulpfile clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 'use strict';
 
 var gulp                    = require('gulp');
-var shell                    = require('gulp-shell');
+var shell                   = require('gulp-shell');
 
 
+gulp.task('jspm:bundle', shell.task([
+  'jspm bundle src/app dist/main.js'
+]))
 
 gulp.task('jspm:bundle-sfx', shell.task([
-  'jspm bundle-sfx app dist/app.sfx.js'
+  'jspm bundle-sfx src/app dist/app.sfx.js'
 ]))
 
 
 gulp.task('serve-changes', () => {
-    gulp.watch('./**/*.js', ['jspm:bundle-sfx']);
+    gulp.watch('./**/*.js', ['jspm:bundle', 'jspm:bundle-sfx']);
 });
 
 gulp.task('start-server', shell.task([
   'live-server'
 ]))
 
-gulp.task('default', ['jspm:bundle-sfx', 'serve-changes', 'start-server']);
+gulp.task('default', ['serve-changes', 'start-server']);
